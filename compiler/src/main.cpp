8f6f41cbb42b569826f1c7780bb6dfcfa97f92ef
Hamster-crab <hamstercrab123@gmail.com>
2025-04-16 20:58:37 +0900

    yajuikuでyajuiku自身をコンパイル

--- compiler/src/main.cpp
+++ compiler/src/main.cpp
@@ -58,6 +58,11 @@ void run()
     system("./MasoRPG");
 }
 
+void yajuiku()
+{
+    system("g++ -std=c++17 compiler/src/main.cpp -o ../../yajuiku");
+}
+
 int main(int argc, char* argv[]) {
     std::vector<std::string> args(argv + 1, argv + argc);
     for (const std::string& arg : args) {
