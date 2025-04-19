947523c5eb868b7d3331243f40e99bdb0513dd3d
Hamster-crab <hamstercrab123@gmail.com>
2025-04-19 09:27:57 +0900

    コンパイラを超イケイケにした

--- compiler/src/main.cpp
+++ compiler/src/main.cpp
@@ -10,6 +10,11 @@
 #include <cstdlib>
 #include <string>
 #include <vector>
+#include <filesystem>
+#include <thread>
+#include <chrono>
+
+namespace fs = std::filesystem;
 
 void help()
 {
@@ -50,24 +55,44 @@ void bootstrap()
     }
 }
 
-void build()
+void build(std::filesystem::path compilerPath)
 {
-    system("g++ -std=c++17 -o ./compiler/run/bin/main src/main.cpp src/Camera2D.cpp $(pkg-config --cflags --libs sdl2 SDL2_image SDL2_ttf SDL2_mixer)");
-    system("cp -r fonts compiler/run/data");
-    system("cp -r image compiler/run/data");
-    system("cp -r music compiler/run/data");
+    std::filesystem::path dataPath = compilerPath / "run" / "data";
+    std::filesystem::path binPath = compilerPath / "run" / "bin" / "main";
+
+    std::string command = "g++ -std=c++17 -o " + binPath.string() +
+        " src/main.cpp src/Camera2D.cpp $(pkg-config --cflags --libs sdl2 SDL2_image SDL2_ttf SDL2_mixer)";
+    system(command.c_str());
+
+    try {
+        std::filesystem::copy("fonts", dataPath / "fonts",
+            std::filesystem::copy_options::recursive | std::filesystem::copy_options::overwrite_existing);
+        std::filesystem::copy("image", dataPath / "image",
+            std::filesystem::copy_options::recursive | std::filesystem::copy_options::overwrite_existing);
+        std::filesystem::copy("music", dataPath / "music",
+            std::filesystem::copy_options::recursive | std::filesystem::copy_options::overwrite_existing);
+    } catch (const std::filesystem::filesystem_error& e) {
+        std::cerr << "コピーエラー: " << e.what() << std::endl;
+    }
 }
 
 void run()
 {
     std::cout << "※開発中" << std::endl;
-    system("sleep 1");
-    system("./compiler/run/bin/main debug");
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    fs::path execPath = fs::current_path() / "compiler" / "run" / "bin" / "main";
+    std::string command = execPath.string() + " debug";
+    system(command.c_str());
 }
 
 void yajuiku()
 {
-    system("g++ -std=c++17 compiler/src/main.cpp -o ./yajuiku");
+    fs::path outputPath = fs::current_path() / "yajuiku";
+    fs::path sourcePath = fs::current_path() / "compiler" / "src" / "main.cpp";
+
+    std::string command = "g++ -std=c++17 " + sourcePath.string() + " -o " + outputPath.string();
+    system(command.c_str());
 }
 
 void install()
@@ -82,21 +107,37 @@ void remove()
 
 void reset()
 {
-    system("rm -rf compiler/run");
-    system("cp -r compiler/sample compiler/run");
+    fs::path compilerPath = fs::current_path() / "compiler";
+    fs::path runPath = compilerPath / "run";
+    fs::path samplePath = compilerPath / "sample";
+
+    try {
+        // compiler/run を削除
+        if (fs::exists(runPath)) {
+            fs::remove_all(runPath);
+        }
+
+        // compiler/sample を compiler/run にコピー
+        fs::copy(samplePath, runPath,
+            fs::copy_options::recursive | fs::copy_options::overwrite_existing);
+    } catch (const fs::filesystem_error& e) {
+        std::cerr << "リセットエラー: " << e.what() << std::endl;
+    }
 }
 
 int main(int argc, char* argv[]) {
+    std::filesystem::path basePath = std::filesystem::current_path();
+    std::filesystem::path compilerPath = basePath / "compiler";
     std::vector<std::string> args(argv + 1, argv + argc);
     for (const std::string& arg : args) {
         if (arg == "bootstrap") bootstrap();
-        else if (arg == "build") build();
+        else if (arg == "build") build(compilerPath);
         else if (arg == "run") run();
         else if (arg == "help") help();
         else if (arg == "yajuiku") yajuiku();
         else if (arg == "builrun")
         {
-            build();
+            build(compilerPath);
             run();
         }
         else if (arg == "install") install();
