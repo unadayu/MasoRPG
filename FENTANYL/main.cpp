3360f57da2e0d6347a99765cd46edc0aef11c064
Kitano-Hideaki <121480505+Hamster-crab@users.noreply.github.com>
2025-05-07 16:23:36 +0900

    Update main.cpp

--- FENTANYL/main.cpp
+++ FENTANYL/main.cpp
@@ -80,7 +80,7 @@ int main(int argc, char* argv[])
         
         if (buildtools)
         {
-            system("./yajuiku build");
+            system("./yajuiku build -n");
             buildtools = false;
         }
 
