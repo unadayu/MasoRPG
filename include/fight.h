3706398cf2cbccfab50f531e056160342311abb8
RainbowPuiPuiMolcar <PuiPuiMolcarlove@outlook.jp>
2025-05-14 17:13:19 +0900

    Update fight.h

--- include/fight.h
+++ include/fight.h
@@ -14,5 +14,24 @@
 #include "function.h"
 
 namespace fightUI {
-  void flame() {}
+  void flame(SDL_Renderer* renderer) {
+    // ステーたす
+    DrawRectangleLines(30, 30, 100, 250, SDL_Color{ 255, 255, 255, 255}, renderer);
+    
+    // 背景
+    DrawRectangleLines(260, 30, 450, 250, SDL_Color{ 255, 255, 255, 255}, renderer);
+    
+    // 偽証
+    DrawRectangleLines(30, 310, 100, 250, SDL_Color{ 255, 255, 255, 255}, renderer);
+    
+    // コマンドの枠
+    // しね
+    DrawRectangleLines(260, 320, 60, 30, SDL_Color{ 255, 255, 255, 255}, renderer);
+    // コ
+    DrawRectangleLines(260, 390, 60, 30, SDL_Color{ 255, 255, 255, 255}, renderer);
+    // チャ
+    DrawRectangleLines(260, 450, 60, 30, SDL_Color{ 255, 255, 255, 255}, renderer);
+    // に
+    DrawRectangleLines(260, 510, 60, 30, SDL_Color{ 255, 255, 255, 255}, renderer);
+  }
 }
