4b657ec3019397603662f55b30aa396b330710cd
RainbowPuiPuiMolcar <PuiPuiMolcarlove@outlook.jp>
2025-05-14 16:16:45 +0900

    Update fight.h

--- include/fight.h
+++ include/fight.h
@@ -1,1 +1,18 @@
+#include <SDL2/SDL.h>
+#include <SDL_ttf.h>
+#include <iostream>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <filesystem>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <chrono>
+#include <thread>
+#include <memory>
+#include "function.h"
 
+namespace fightUI {
+  void flame() {}
+}
